df48cd3c33fe6d3e2115652c4860806ccb8db3cf
RianCQ <rianquintanilha5@gmail.com>
2025-12-20 21:34:58 -0300

    Altera a rota principal para exibir a tela de login e mantém a rota de cadastro

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import SchoolRegistration from './pages/Cadastro/Cadastro';
+import LoginScreen from './pages/Login/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SchoolRegistration />} />
+        <Route path="/" element={<LoginScreen/>} />
+        <Route path="/cadastro" element={<SchoolRegistration/>} />
       </Routes>
     </Router>
   );
